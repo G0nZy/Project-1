efb4d7cbe12b2989eb58e847012226885496a8f1
G0nZy <knicksfan1297@gmail.com>
2020-04-09 21:23:54 -0400

    resolutions

---
+++ docs/js/resolutions.js
@@ -0,0 +1,27 @@
+
+
+
+/* Example Javascript Below! Still Under Construction ! 
+
+
+
+/* For mobile & phones 
+
+@media screen and (max-width: 540px) {
+    .view {
+        width: 400px;
+    }
+}
+
+            */
+
+
+/* For Tablets 
+@media screen and (min-width: 540px) and (max-width: 780px) {
+    .view {
+        width: 600px;
+    }
+}
+
+
+*/
